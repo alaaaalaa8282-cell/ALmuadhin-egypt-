95ae47fc43fe6c06066a44762c3321b123b09129
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-18 03:54:52 +0200

    Create ZekrReceiver.kt

---
+++ app/src/main/java/com/example/almuadhin/alarm/ZekrReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.almuadhin.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ZekrReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        context.startService(Intent(context, ZekrService::class.java))
+    }
+}
