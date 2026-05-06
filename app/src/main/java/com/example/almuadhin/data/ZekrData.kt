9cded516d798cbddf6cd869417d736aa11dc2586
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-06 15:01:58 +0300

    Refactor ZekrData to improve readability

--- app/src/main/java/com/example/almuadhin/data/ZekrData.kt
+++ app/src/main/java/com/example/almuadhin/data/ZekrData.kt
@@ -6,10 +6,14 @@ data class Zekr(val name: String, val resId: Int)
 
 object ZekrData {
     val zekrList = listOf(
-        Zekr("سبحان الله وبحمده",        R.raw.sobhanallah_wabehamdeh),
-        Zekr("الحمد لله",                R.raw.alhamdo_lelah),
-        Zekr("اللهم لك الحمد",           R.raw.allahom_lk_alhamd),
+        Zekr("سبحان الله وبحمده", R.raw.sobhanallah_wabehamdeh),
+        Zekr("الحمد لله", R.raw.alhamdo_lelah),
+        Zekr("اللهم لك الحمد", R.raw.allahom_lk_alhamd),
         Zekr("لا حول ولا قوة إلا بالله", R.raw.lahawla_wlaqowat),
-        Zekr("ربنا اغفر لي",             R.raw.rbna_ighfer_li)
+        Zekr("ربنا اغفر لي", R.raw.rbna_ighfer_li),
+        Zekr("اللهم صل على الحبيب", R.raw.nozaker_salt_ala_habib),
+        Zekr("آية الأحزاب", R.raw.ayah_elahzab)
     )
+
+    val sobhanallah_wabehemden = "سبحان الله وبحمده"
 }
